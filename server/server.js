b33b1545d6b5bb98d5dab7a498e0653143bf6127
salperen <sal.alperen1@gmail.com>
2023-11-24 18:11:12 +0100

    Add basic dependencies and routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log("server");
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+dotenv.config();
+const app = express();
+// imports
+
+// middlewares
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
+
+// errors
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
