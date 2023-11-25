07c226df75c2564efcd7beae29c044239f6543af
salperen <sal.alperen1@gmail.com>
2023-11-26 00:40:22 +0100

    Add new dependencies to project
    Added express async errors to handle any kind of async errors
    installed mongoose for Object modeling mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+import "express-async-errors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import { recipeRoute } from "./routes/recipesRoute.js";
+import { notFound } from "./middlewares/notFound.js";
 dotenv.config();
 const app = express();
 // imports
@@ -12,8 +15,17 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.send("Hi there");
 });
+// recipe route
+app.use("/api/v1/recipes", recipeRoute);
 
 // errors
+app.use(notFound);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+try {
+  // first connect to DB after setting up
+  app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+} catch (error) {
+  console.log(error);
+}
