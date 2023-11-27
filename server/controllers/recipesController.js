0765bb04ea30bf6a6d12904e4ebc75151df7da0b
salperen <sal.alperen1@gmail.com>
2023-11-27 19:59:43 +0100

    Add recipe REST functionality

--- server/controllers/recipesController.js
+++ server/controllers/recipesController.js
@@ -1,27 +1,39 @@
+import Recipe from "../models/recipeModel.js";
+import { StatusCodes } from "http-status-codes";
+
 // get recipes
 const getRecipes = async (req, res) => {
-  throw new Error(`Something  went wrong`);
-  res.status(200).send("Get Recipes");
+  const recipes = await Recipe.find({});
+  res.status(StatusCodes.OK).send({ recipes });
 };
 
 // add recipe
 const addRecipe = async (req, res) => {
-  res.status(200).send("Add Recipe");
+  console.log(req.body);
+  const newRecipe = await Recipe.create(req.body);
+  res.status(StatusCodes.CREATED).send({ msg: "recipe created successfully" });
 };
 
 // get recipe
 const getSingleRecipe = async (req, res) => {
-  res.status(200).send("Get Recipe");
+  const { id } = req.params;
+  const recipe = await Recipe.findById(id);
+  res.status(StatusCodes.OK).send({ recipe });
 };
 
 // update recipes
 const updateRecipe = async (req, res) => {
-  res.status(200).send("update recipe");
+  const { id } = req.params;
+  console.log(req.body);
+  await Recipe.findOneAndUpdate({ _id: id }, req.body, { new: true });
+  res.status(StatusCodes.OK).json({ msg: "Updated successfully" });
 };
 
 // delete recipes
 const deleteRecipe = async (req, res) => {
-  res.status(200).send("delete recipe");
+  const { id } = req.params;
+  await Recipe.findByIdAndDelete(id);
+  res.status(200).send({ msg: "deleted successfully" });
 };
 
 export { getRecipes, addRecipe, getSingleRecipe, updateRecipe, deleteRecipe };
