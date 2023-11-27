f1ffbe468efb0ec339f91f30398706e7c34bd196
salperen <sal.alperen1@gmail.com>
2023-11-27 19:58:44 +0100

    Add errorHandler and body parser

--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ import morgan from "morgan";
 import { recipeRoute } from "./routes/recipesRoute.js";
 import { notFound } from "./middlewares/notFound.js";
 import { connectDB } from "./db/connectDB.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandler.js";
 dotenv.config();
 const app = express();
 // imports
 
 // middlewares
+app.use(express.json());
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 // routes
@@ -21,6 +23,7 @@ app.use("/api/v1/recipes", recipeRoute);
 
 // errors
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
