29380acc504471154c83246af49f6402a92a9993
salperen <sal.alperen1@gmail.com>
2023-11-28 23:27:37 +0100

    Add initial post recipe validation

--- server/routes/recipesRoute.js
+++ server/routes/recipesRoute.js
@@ -6,11 +6,11 @@ import {
   getSingleRecipe,
   updateRecipe,
 } from "../controllers/recipesController.js";
-
+import { recipePostValidation } from "../middlewares/validations.js";
 export const recipeRoute = Router();
 
 // get recipes
-recipeRoute.route("/").get(getRecipes).post(addRecipe);
+recipeRoute.route("/").get(getRecipes).post(recipePostValidation, addRecipe);
 recipeRoute
   .route("/:id")
   .get(getSingleRecipe)
