9086db405844c957438b2667d19ca3e76940e2df
salperen <sal.alperen1@gmail.com>
2024-01-01 22:09:06 +0100

    Add auth controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,15 +1,47 @@
 import { StatusCodes } from "http-status-codes";
-
+import { comparePassword, hashPassword } from "../utils/passwordHashing.js";
+import User from "../models//userModel.js";
+import jwt from "jsonwebtoken";
+import { createJWT } from "../utils/jwt.js";
+import { Unauthenticated } from "../errors/errorClasses.js";
 const register = async (req, res) => {
-  res.status(StatusCodes.OK).send("Register");
+  const { password } = req.body;
+  const hashedPass = await hashPassword(password);
+  req.body.password = hashedPass;
+  const user = await User.create(req.body);
+  res
+    .status(StatusCodes.CREATED)
+    .send({ msg: "user created successfully!", user });
 };
 
 const login = async (req, res) => {
+  const { email, password } = req.body;
+  const specificUser = await User.findOne({ email: email });
+  const isPassCorrect = await comparePassword(password, specificUser.password);
+  if (!specificUser || !isPassCorrect) {
+    throw new Unauthenticated("invalid credentials");
+  }
+  // create jwt and attach the cookie
+  const token = createJWT({ user: specificUser.id });
+  console.log(token);
+
+  const oneDayInMs = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDayInMs),
+    secure: process.env.NODE_ENV === "production",
+  });
+
   res.status(StatusCodes.OK).send("Login");
 };
 
 const logout = async (req, res) => {
-  res.status(StatusCodes.OK).send("Logut");
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).send("Logout");
 };
 
 export { register, login, logout };
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { login, logout, register } from "../controllers/authController.js";
+import {
+  loginPostValidation,
+  registerPostValidation,
+} from "../middlewares/validations.js";
 
 export const authRoute = Router();
 
-authRoute.post("/register", register);
-authRoute.post("/login", login);
+authRoute.post("/register", registerPostValidation, register);
+authRoute.post("/login", loginPostValidation, login);
 authRoute.get("/logout", logout);
