48346e2a04d3d144b6f6db475c6aa9f9a74d5e05
salperen <sal.alperen1@gmail.com>
2024-01-03 21:51:29 +0100

    Add form row component

---
+++ client/src/components/FormRow.tsx
@@ -0,0 +1,21 @@
+interface FormRowProp {
+  name: string;
+  type: string;
+  defaultValue: string;
+  labelDisplay?: string;
+}
+
+const FormRow: React.FC<FormRowProp> = ({
+  name,
+  type,
+  defaultValue,
+  labelDisplay,
+}) => {
+  return (
+    <div className='form-row'>
+      <label htmlFor={name}>{labelDisplay || name}</label>
+      <input type={type} name={type} id={type} defaultValue={defaultValue} />
+    </div>
+  );
+};
+export default FormRow;
