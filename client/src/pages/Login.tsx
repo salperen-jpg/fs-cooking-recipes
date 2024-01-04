6ef53a87c34f2bb5d41c2047e85e964e805e97b2
Salih Alperen <sal.alperen1@gmail.com>
2024-01-05 00:40:14 +0100

    Add Login functionality

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,4 +1,35 @@
+import FormRow from '../components/FormRow';
+import { Form, redirect } from 'react-router-dom';
+import { customFetch } from '../utils/customFetch';
+import { toast } from 'react-toastify';
+import { Wrapper } from './Register';
+
+export const action = async ({ request }: any) => {
+  const formData = await request.formData();
+  const loginData = Object.fromEntries(formData);
+  try {
+    await customFetch.post('/auth/login', loginData);
+    toast.success('Successful!');
+    return redirect('/recipes');
+  } catch (error: any) {
+    return toast.error(error?.response?.data?.msg);
+  }
+};
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Wrapper>
+      <Form method='POST' className='form'>
+        <div className='form-cen'>
+          <h4>login</h4>
+          <FormRow name='email' type='email' defaultValue='john@gmail.com' />
+          <FormRow name='password' type='password' defaultValue='12345678' />
+          <button type='submit' className='btn'>
+            register
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 export default Login;
