eaa460bef9c4e9f4f925c594e0bf8ea5f261147b
salperen <sal.alperen1@gmail.com>
2024-01-04 20:02:36 +0100

    Fix login page bug

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,4 @@
 export { default as ErrorPage } from "./Error";
 export { default as LandingPage } from "./Landing";
-export { default as LoginPage } from "./Landing";
+export { default as LoginPage } from "./Login";
 export { default as RegisterPage } from "./Register";
