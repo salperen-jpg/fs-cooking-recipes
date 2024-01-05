2fa24040943ccf7cac7b0fcf2842531dedad9ad5
salperen <sal.alperen1@gmail.com>
2024-01-05 21:33:42 +0100

    Add recipe layout and its components

--- client/src/pages/RecipesLayout.tsx
+++ client/src/pages/RecipesLayout.tsx
@@ -1,8 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../components";
+import Sidebar from "../components/Sidebar";
 
 const RecipesDashboardLayout = () => {
   return (
     <>
+      <Navbar />
+      {/* sidebar out of flow does not matter where you place it  */}
+      <Sidebar />
       <Outlet />
     </>
   );
