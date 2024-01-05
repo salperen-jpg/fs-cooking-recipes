c5bf556b96aef93fd9f273273ab631bed120b5d2
salperen <sal.alperen1@gmail.com>
2024-01-05 22:45:22 +0100

    Add initial navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,4 +1,18 @@
+import styled from "styled-components";
+import { Navlinks } from ".";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Wrapper>
+      <div className="wrapper-center">
+        <h3>logo coming up</h3>
+        <Navlinks />
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.nav`
+  background-color: red;
+`;
 export default Navbar;
