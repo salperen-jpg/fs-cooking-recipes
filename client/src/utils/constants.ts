3415dc9d823e287e1ed5c696d8a588d5864339e0
salperen <sal.alperen1@gmail.com>
2024-01-05 22:10:47 +0100

    Add navlinks util

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,24 @@
+import { nanoid } from "nanoid";
+
+export const navbarLinks = [
+  {
+    id: nanoid(),
+    text: "recipes",
+    path: "recipes",
+  },
+  {
+    id: nanoid(),
+    text: "add recipe",
+    path: "addRecipe",
+  },
+  {
+    id: nanoid(),
+    text: "admin",
+    path: "admin",
+  },
+  {
+    id: nanoid(),
+    text: "profile",
+    path: "profile",
+  },
+];
