8a932b576afacb9f5af751c07a42528f4932c8a3
salperen <sal.alperen1@gmail.com>
2024-01-05 22:44:32 +0100

    Add shared layout and context

--- client/src/pages/RecipesLayout.tsx
+++ client/src/pages/RecipesLayout.tsx
@@ -1,15 +1,31 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components";
 import Sidebar from "../components/Sidebar";
+import { createContext, useState } from "react";
+
+const initialContextState = {
+  isSidebarOpen: false,
+  toggleSidebar() {},
+};
+
+const RecipeContext = createContext(initialContextState);
 
 const RecipesDashboardLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <>
-      <Navbar />
+    <RecipeContext.Provider value={{ isSidebarOpen, toggleSidebar }}>
       {/* sidebar out of flow does not matter where you place it  */}
       <Sidebar />
-      <Outlet />
-    </>
+      <Navbar />
+      <div className="wrapper-center">
+        <Outlet context={{ isSidebarOpen }} />
+      </div>
+    </RecipeContext.Provider>
   );
 };
 export default RecipesDashboardLayout;
