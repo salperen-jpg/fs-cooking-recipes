56ae7d61f207690c54f5b5e96777c741284ff6bb
salperen <sal.alperen1@gmail.com>
2024-01-06 22:26:10 +0100

    Change return format

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,10 @@
 import User from "../models/userModel.js";
+import { StatusCodes } from "http-status-codes";
 
 const getUser = async (req, res) => {
-  console.log(req.user);
   const user = await User.findOne({ _id: req.user.user });
   const userPassExcluded = user.excludePass();
-  res.send({ user: userPassExcluded });
+  res.status(StatusCodes.OK).json({ user: userPassExcluded });
 };
 
 const updateUser = async (req, res) => {
