012c32978901ab84fd29ca5da4de28ccf1d77e29
salperen <sal.alperen1@gmail.com>
2024-01-09 23:24:13 +0100

    Add loading component

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,4 +1,21 @@
+import styled from "styled-components";
+
 const Loading = () => {
-  return <div>Loading</div>;
+  return <Wrapper className="loading"></Wrapper>;
 };
+
+const Wrapper = styled.div`
+  width: 6rem;
+  height: 6rem;
+  border-radius: 50%;
+  border: 5px solid var(--grey-700);
+  border-top-color: var(--primary-300);
+  animation: spinner 0.6s linear infinite;
+
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 export default Loading;
