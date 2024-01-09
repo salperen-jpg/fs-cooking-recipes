dbb28a5de0c284306c5216d11f7c1545d446f88e
salperen <sal.alperen1@gmail.com>
2024-01-09 23:12:59 +0100

    Add editing functionality

--- client/src/pages/EditRecipe.tsx
+++ client/src/pages/EditRecipe.tsx
@@ -18,8 +18,21 @@ export const loader = async (data: any) => {
   return null;
 };
 
-export const action = async () => {
-  return null;
+export const action = async ({ request, params }: any) => {
+  const { id } = params;
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const ingredientArr = formData.get("ingredients").split(",");
+  data.ingredients = ingredientArr;
+  try {
+    const response = await customFetch.patch(`/recipes/${id}`, data);
+    console.log(response);
+    toast.success("Edited successfully !");
+    return redirect("/recipes");
+  } catch (error: any) {
+    toast.error(error?.response?.data?.msg);
+    return null;
+  }
 };
 
 const EditRecipe = () => {
@@ -27,24 +40,24 @@ const EditRecipe = () => {
   const { name, cookingTime, ingredients, servings, mealCategory } = recipe;
   return (
     <Wrapper>
-      <Form method='POST'>
+      <Form method="POST">
         <h4>Edit Recipe</h4>
-        <div className='form-center'>
-          <FormRow type='text' name='name' defaultValue={name} />
+        <div className="form-center">
+          <FormRow type="text" name="name" defaultValue={name} />
           <FormRow
-            type='text'
-            name='cookingTime'
+            type="text"
+            name="cookingTime"
             defaultValue={cookingTime}
-            labelDisplay='cooking time'
+            labelDisplay="cooking time"
           />
           <FormRow
-            type='number'
-            name='servings'
+            type="number"
+            name="servings"
             defaultValue={servings.toString()}
           />
           <FormSelect
-            name='mealCategory'
-            labelDisplay='meal category'
+            name="mealCategory"
+            labelDisplay="meal category"
             list={[
               "vegetarian",
               "breakfast",
@@ -56,14 +69,14 @@ const EditRecipe = () => {
             defaultValue={mealCategory}
           />
           <FormTextarea
-            name='ingredients'
+            name="ingredients"
             placeholder="'Please type ingredient comma separated!'"
             defaultValue={ingredients.join(",")}
           >
             {ingredients.join(",")}
           </FormTextarea>
-          <button type='submit' className='btn submit-btn'>
-            Create recipe
+          <button type="submit" className="btn submit-btn">
+            Edit recipe
           </button>
         </div>
       </Form>
