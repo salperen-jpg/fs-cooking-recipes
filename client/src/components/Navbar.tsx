a0fe3d4e46a6a88529830fb8800d9cf7772b36fe
Salih Alperen <sal.alperen1@gmail.com>
2024-01-25 21:13:42 +0100

    Finish navbar styles

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -19,23 +19,23 @@ const Navbar = () => {
 
   return (
     <nav className="bg-emerald-200 flex items-center py-6">
-      <div className="px-4 mx-auto w-11/12 max-w-7xl ">
-        <header className="flex justify-between items-center  w-full  ">
+      <div className="px-4 mx-auto w-11/12 max-w-7xl md:flex justify-between">
+        <header className="flex justify-between items-center  w-full  md:w-auto">
           <h3>logo</h3>
-          <div className="flex items-center justify-between gap-4 right-side ">
-            <button className="btn hamburger-btn" onClick={toggleSidebar}>
+          <div className="flex items-center justify-between gap-4  md:hidden">
+            <button className="btn hamburger-btn " onClick={toggleSidebar}>
               <FaBarsStaggered />
             </button>
             <UserContainer />
           </div>
         </header>
         <div
-          className="nav-links-container overflow-hidden transition-all delay-150"
+          className="nav-links-container overflow-hidden transition-all delay-150 md:!h-auto"
           ref={navLinksOuterContainer}
           style={outerStyles}
         >
           <div
-            className="nav-links pt-4 flex flex-col gap-4 "
+            className="nav-links pt-4 flex flex-col gap-4 md:flex-row3 md:flex-row "
             ref={navLinksInnerContainer}
           >
             {navbarLinks.map((navlink) => {
@@ -52,7 +52,7 @@ const Navbar = () => {
             })}
           </div>
         </div>
-        <div className="user-container-bigger-screen hidden">
+        <div className="user-container-bigger-screen hidden md:block">
           <UserContainer />
         </div>
       </div>
@@ -60,91 +60,4 @@ const Navbar = () => {
   );
 };
 
-const Wrapper = styled.nav`
-  background: var(--primary-300);
-  padding: 1rem 0;
-  .nav-center {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-  }
-  header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-  .right-side {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-  }
-  .hamburger-btn {
-    background-color: transparent;
-    svg {
-      font-size: 1.5rem;
-    }
-  }
-  .hamburger-btn:hover {
-    border-color: transparent;
-    transform: rotate(90deg);
-  }
-  /* .user-container {
-    display: none;
-  } */
-  .nav-links-container {
-    overflow: hidden;
-    transition: all 0.3s linear;
-  }
-  .nav-links {
-    padding-top: 1rem;
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-  }
-  .nav-link {
-    display: block;
-    text-transform: capitalize;
-    font-size: 0.775rem;
-    letter-spacing: var(--spacing);
-    font-weight: 600;
-    color: var(--white);
-    cursor: pointer;
-  }
-  .user-container-bigger-screen {
-    display: none;
-  }
-  /* come back to fix value later on */
-  @media (min-width: 992px) {
-    & {
-      padding: 0;
-      display: flex;
-      align-items: center;
-      height: var(--nav-height);
-    }
-
-    .nav-center {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    }
-    .right-side {
-      display: none;
-    }
-    .nav-links-container {
-      height: auto !important;
-    }
-    .nav-links {
-      padding: 0;
-      flex-direction: row;
-    }
-    .nav-link {
-    }
-    .hamburger-btn {
-      display: none;
-    }
-    .user-container-bigger-screen {
-      display: block;
-    }
-  }
-`;
 export default Navbar;
