55452adaa270c890e5a6529d0d235834e71cb724
salperen <sal.alperen1@gmail.com>
2024-02-04 20:09:36 +0100

    Add delete edit icons

--- client/src/pages/Recipe.tsx
+++ client/src/pages/Recipe.tsx
@@ -3,6 +3,8 @@ import { RecipeProperty, Title } from "../components";
 import { customFetch } from "../utils/customFetch";
 import { Form, Link, redirect, useLoaderData } from "react-router-dom";
 import IRecipe from "../models/recipe.modal";
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 
 export const loader = async (data: any) => {
   const { id } = data.params;
@@ -31,14 +33,22 @@ const Recipe = () => {
     <section className="wrapper-center">
       <Link
         to={"/recipes"}
-        className="bg-emerald-400 capitalize font-bolder tracking-wider	p-4 rounded-md text-white -translate-y-6 inline-block w-auto hover:bg-emerald-600 duration-150	"
+        className="bg-emerald-400 capitalize font-bolder tracking-wider	p-4 rounded-md text-white -translate-y-6 inline-block w-auto hover:bg-emerald-600 duration-150"
       >
-        back home
+        back
       </Link>
       <Title title={name} />
       <div className="grid gap-4 md:grid-cols-2 md:gap-12">
         <div>
-          <img src={recipeAvatar} alt={name} />
+          <img
+            src={recipeAvatar}
+            alt={name}
+            style={{
+              aspectRatio: "1/1",
+              maxWidth: "500px",
+              objectFit: "cover",
+            }}
+          />
         </div>
         <div>
           <RecipeProperty property="name" value={name} />
@@ -56,13 +66,16 @@ const Recipe = () => {
                 .join(",")}
             </span>
           </div>
-          <div className="mt-8 flex gap-8">
-            <Link to={`../editRecipe/${_id}`} className="btn">
-              edit
+          <div className="mt-8 flex gap-4">
+            <Link to={`../editRecipe/${_id}`} className="btn text-xl">
+              <MdEdit />
             </Link>
             <Form method="POST" action={`../deleteRecipe/${_id}`}>
-              <button type="submit" className="btn bg-red-400 hover:bg-red-600">
-                delete
+              <button
+                type="submit"
+                className="btn bg-red-400 hover:bg-red-600 text-xl"
+              >
+                <MdDelete />
               </button>
             </Form>
           </div>
