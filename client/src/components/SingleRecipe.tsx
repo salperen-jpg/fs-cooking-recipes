85283071e6d913c4d074f9dc69b2b39bbee5541c
salperen <sal.alperen1@gmail.com>
2024-02-04 20:09:59 +0100

    Add to favorites

--- client/src/components/SingleRecipe.tsx
+++ client/src/components/SingleRecipe.tsx
@@ -1,9 +1,12 @@
 import IRecipe from "../models/recipe.modal";
 import { FaPlateWheat, FaClock } from "react-icons/fa6";
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdOutlineFavorite } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { RecipeProperty } from ".";
-
+import { customFetch } from "../utils/customFetch";
+import { toast } from "react-toastify";
+import defaultImg from "../assets/default_food.jpg";
+import useFavorites from "../hooks/useFavorites";
 const SingleRecipe: React.FC<IRecipe> = ({
   _id,
   name,
@@ -12,24 +15,46 @@ const SingleRecipe: React.FC<IRecipe> = ({
   servings,
   mealCategory,
 }) => {
+  const { isLoading, favorites } = useFavorites();
+
+  const addToFavorites = async () => {
+    try {
+      const response = await customFetch.post("/favorites", { recipeId: _id });
+      console.log(response);
+      toast.success(response.data.msg);
+    } catch (error: any) {
+      toast.error(error?.response?.data?.msg);
+    }
+  };
+
+  const isIncludedInFav = favorites?.find((fav) => fav._id === _id);
+
   return (
     <article className="relative bg-white my-8 rounded-md shadow-sm hover:shadow-md  duration-150 hover:scale-105">
       <button
         type="button"
         className="text-red-700 absolute top-1 right-1 text-xl"
-        onClick={() => console.log("favorited !")}
+        onClick={isIncludedInFav ? undefined : addToFavorites}
       >
-        <MdFavoriteBorder />
+        {isIncludedInFav ? <MdOutlineFavorite /> : <MdFavoriteBorder />}
       </button>
       <div>
-        <img
-          src={recipeAvatar}
-          alt={name}
-          className="w-32 h-32 rounded-full block mx-auto -translate-y-2/4 border-emerald-400 border-4	"
-        />
+        {recipeAvatar ? (
+          <img
+            src={recipeAvatar}
+            alt={name}
+            className="w-32 h-32 rounded-full block mx-auto -translate-y-2/4 border-emerald-400 border-4	"
+          />
+        ) : (
+          <img
+            src={defaultImg}
+            alt={name}
+            className="w-32 h-32 rounded-full block mx-auto -translate-y-2/4 border-emerald-400 border-4	"
+          />
+        )}
       </div>
       <div className="-translate-y-8 px-4">
-        <h3 className="text-red-600 font-bold text-center capitalize tracking-wide">
+        <h3 className="text-red-600 mb-4 font-bold text-center capitalize tracking-wide">
           {name}
         </h3>
         <RecipeProperty property="category" value={mealCategory} />
