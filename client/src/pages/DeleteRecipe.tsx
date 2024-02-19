d717a89b1c01f07990947b5cd063e8dc725316a6
salperen <sal.alperen1@gmail.com>
2024-02-19 22:23:55 +0100

    Add deleteQuery

--- client/src/pages/DeleteRecipe.tsx
+++ client/src/pages/DeleteRecipe.tsx
@@ -1,14 +1,18 @@
 import { redirect } from "react-router-dom";
 import { customFetch } from "../utils/customFetch";
 import { toast } from "react-toastify";
+import { QueryClient } from "@tanstack/react-query";
 
-export const action = async ({ params }: any) => {
-  const { id } = params;
-  try {
-    await customFetch.delete(`/recipes/${id}`);
-    toast.success("deleted successfully !");
-    return redirect("/recipes");
-  } catch (error: any) {
-    return toast.error(error?.response?.data?.msg);
-  }
+export const action = (queryClient: QueryClient) => {
+  return async ({ params }: any) => {
+    const { id } = params;
+    try {
+      await customFetch.delete(`/recipes/${id}`);
+      await queryClient.invalidateQueries({ queryKey: ["recipes"] });
+      toast.success("deleted successfully !");
+      return redirect("/recipes");
+    } catch (error: any) {
+      return toast.error(error?.response?.data?.msg);
+    }
+  };
 };
