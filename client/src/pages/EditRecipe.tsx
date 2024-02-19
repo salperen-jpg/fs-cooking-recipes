56b5ae93272d2986ce16d1d0e0d6b389e9ae137c
salperen <sal.alperen1@gmail.com>
2024-02-19 22:24:09 +0100

    Add edit recipe query

--- client/src/pages/EditRecipe.tsx
+++ client/src/pages/EditRecipe.tsx
@@ -3,39 +3,45 @@ import { FormRow, FormSelect, FormTextarea } from "../components";
 import { customFetch } from "../utils/customFetch";
 import { toast } from "react-toastify";
 import IRecipe from "../models/recipe.modal";
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { recipeQuery } from "./Recipe";
 
-export const loader = async (data: any) => {
-  const { id } = data.params;
-  try {
-    const {
-      data: { recipe },
-    } = await customFetch.get(`/recipes/${id}`);
-    return recipe;
-  } catch (error: any) {
-    console.log(error?.response?.data?.msg);
-  }
-  return null;
+export const loader = (queryClient: QueryClient) => {
+  return async (data: any) => {
+    const { id } = data.params;
+    try {
+      await queryClient.ensureQueryData(recipeQuery(id));
+      return id;
+    } catch (error: any) {
+      console.log(error?.response?.data?.msg);
+    }
+    return null;
+  };
 };
 
-export const action = async ({ request, params }: any) => {
-  const { id } = params;
-  const formData = await request.formData();
-  const ingredientArr = formData.get("ingredients").split(",");
-  try {
-    const response = await customFetch.patch(`/recipes/${id}`, formData);
-    toast.success(response.data.msg);
-    return redirect("/recipes");
-  } catch (error: any) {
-    toast.error(error?.response?.data?.msg);
-    return null;
-  }
+export const action = (queryClient: QueryClient) => {
+  return async ({ request, params }: any) => {
+    const { id } = params;
+    const formData = await request.formData();
+    const ingredientArr = formData.get("ingredients").split(",");
+    try {
+      const response = await customFetch.patch(`/recipes/${id}`, formData);
+      toast.success(response.data.msg);
+      await queryClient.invalidateQueries({ queryKey: ["recipes"] });
+      return redirect("/recipes");
+    } catch (error: any) {
+      toast.error(error?.response?.data?.msg);
+      return null;
+    }
+  };
 };
 
 const EditRecipe = () => {
-  const recipe = useLoaderData() as IRecipe;
-  const { name, cookingTime, ingredients, servings, mealCategory } = recipe;
+  const id = useLoaderData() as string;
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
+  const { data } = useQuery(recipeQuery(id));
+  const { name, cookingTime, ingredients, servings, mealCategory } = data;
   return (
     <section>
       <Form method="POST" encType="multipart/form-data" className="form">
