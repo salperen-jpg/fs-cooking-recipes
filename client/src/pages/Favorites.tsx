45d111e9cdf53ba51e826039d07ac44ab0ade098
salperen <sal.alperen1@gmail.com>
2024-02-19 22:24:36 +0100

    Add favorites query

--- client/src/pages/Favorites.tsx
+++ client/src/pages/Favorites.tsx
@@ -3,21 +3,26 @@ import { customFetch } from "../utils/customFetch";
 import { Link, useLoaderData } from "react-router-dom";
 import IRecipe from "../models/recipe.modal";
 import { SingleRecipe, Title } from "../components";
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { favoritesQuery } from "../hooks/useFavorites";
 
-export const loader = async () => {
-  try {
-    const response = await customFetch("/favorites");
-    return response.data.favorites;
-  } catch (error: any) {
-    toast.error(error?.response?.data?.msg);
-    return null;
-  }
+export const loader = (queryClient: QueryClient) => {
+  return async () => {
+    try {
+      const response = await customFetch("/favorites");
+      await queryClient.ensureQueryData(favoritesQuery());
+      return response.data.favorites;
+    } catch (error: any) {
+      toast.error(error?.response?.data?.msg);
+      return null;
+    }
+  };
 };
 
 const Favorites = () => {
-  const favorites = useLoaderData() as IRecipe[];
-
-  if (favorites.length < 1) {
+  // const favorites = useLoaderData() as IRecipe[];
+  const { data: favorites } = useQuery(favoritesQuery());
+  if (favorites!.length < 1) {
     return (
       <>
         <Title title="Favorites" />
@@ -35,7 +40,7 @@ const Favorites = () => {
     <>
       <Title title="Favorites" />
       <section className="grid  md:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-8 ">
-        {favorites.map((recipe) => {
+        {favorites?.map((recipe) => {
           return <SingleRecipe key={recipe._id} {...recipe} favorite />;
         })}
       </section>
