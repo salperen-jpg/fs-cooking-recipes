b779e80de58b9b15c448d681b709f94fb10626ee
salperen <sal.alperen1@gmail.com>
2024-02-19 22:25:52 +0100

    Add getFavorites controller fully

--- server/controllers/favoritesController.js
+++ server/controllers/favoritesController.js
@@ -4,16 +4,18 @@ import Recipe from "../models/recipeModel.js";
 
 const getFavorites = async (req, res) => {
   const favoriteRecipes = await Favorites.find({ createdBy: req.user.user });
-  const currentUserFavoritesPromises = favoriteRecipes.map(
-    async (fav) => await Recipe.findById(fav.recipeId)
-  );
+  const currentUserFavoritesPromises = favoriteRecipes.map(async (fav) => {
+    return await Recipe.findById(fav.recipeId);
+  });
   try {
     const currentUserFavorites = await Promise.all(
       currentUserFavoritesPromises
     );
     return res.status(StatusCodes.OK).json({ favorites: currentUserFavorites });
   } catch (error) {
-    return res.status(StatusCodes.OK), json({ msg: "Something went wrong" });
+    return (
+      res.status(StatusCodes.BAD_REQUEST), json({ msg: "Something went wrong" })
+    );
   }
 };
 
@@ -25,14 +27,12 @@ const addToFavorites = async (req, res) => {
   });
   const favoriteRecipe = { recipeId, createdBy: req.user.user };
   await Favorites.create(favoriteRecipe);
-  console.log(currentUserFavoriteRecipes);
   res.status(StatusCodes.OK).json({ msg: "Added to favorites!" });
 };
 
 const deleteFavorite = async (req, res) => {
   const { id } = req.params;
   const removedOne = await Favorites.findOneAndDelete({ recipeId: id });
-  console.log(removedOne);
   res.status(StatusCodes.OK).json({ msg: "removed succesfully!" });
 };
 
