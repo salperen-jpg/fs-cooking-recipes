f456e7adf4ee899553bad6af6d7bb2077fc428e3
salperen <sal.alperen1@gmail.com>
2024-02-19 22:24:47 +0100

    Add profile query

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -3,22 +3,26 @@ import { FormRow } from "../components";
 import { Form } from "react-router-dom";
 import { customFetch } from "../utils/customFetch";
 import { toast } from "react-toastify";
+import { QueryClient } from "@tanstack/react-query";
 
-export const action = async ({ request }: any) => {
-  const formData = await request.formData();
-  // const newUserData = Object.fromEntries(formData);
-  console.log(formData);
-  const avatar = formData.get("avatar");
-  if (avatar.size > 800000) {
-    toast.error("Avatar can not be bigger than 8MB!");
-    return null;
-  }
-  try {
-    const response = await customFetch.patch("/user/updateUser", formData);
-    return toast.success(response.data.msg);
-  } catch (error: any) {
-    return toast.error(error?.response?.data?.msg);
-  }
+export const action = (queryClient: QueryClient) => {
+  return async ({ request }: any) => {
+    const formData = await request.formData();
+    // const newUserData = Object.fromEntries(formData);
+    console.log(formData);
+    const avatar = formData.get("avatar");
+    if (avatar.size > 800000) {
+      toast.error("Avatar can not be bigger than 8MB!");
+      return null;
+    }
+    try {
+      const response = await customFetch.patch("/user/updateUser", formData);
+      await queryClient.invalidateQueries({ queryKey: ["user"] });
+      return toast.success(response.data.msg);
+    } catch (error: any) {
+      return toast.error(error?.response?.data?.msg);
+    }
+  };
 };
 
 const Profile = () => {
